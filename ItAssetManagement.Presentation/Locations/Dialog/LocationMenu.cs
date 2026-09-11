6ef97e013d554bca3ffa9e8151a1f1fb7b01426f
Ghraz-hub <tomas.dan.carlsson@hotmail.com>
2026-09-11 15:35:26 +0200

    Add ILocationDialog, LocationDialog and LocationMenu

---
+++ ItAssetManagement.Presentation/Locations/Dialog/LocationMenu.cs
@@ -0,0 +1,38 @@
+﻿namespace ItAssetManagement.Presentation.Locations.Dialog;
+
+internal class LocationMenu(ILocationDialog locationDialog)
+{
+    private bool _isRunning = true;
+    public void MenuDialog()
+    {
+        do
+        {
+            Console.WriteLine("#### LOCATIONMENU ####");
+            Console.WriteLine("[1] Add Location");
+            Console.WriteLine("[2] View All ");
+            Console.WriteLine("[0] Quit");
+
+            var input = Console.ReadLine();
+
+            switch (input)
+            {
+                case "1":
+                    locationDialog.ShowLocationDialog();
+
+                    break;
+                case "2":
+                    locationDialog.ShowAllLocations();
+                    break;
+                case "0":
+                    _isRunning = false;
+                    break;
+                default:
+                    Console.WriteLine("You must enter a valid option!");
+                    Console.ReadKey();
+                    break;
+            }
+
+        }
+        while (_isRunning);
+    }
+}
