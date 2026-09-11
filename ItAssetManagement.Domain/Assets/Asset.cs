f440acb7c8303214c7849372786e09ea2b627cab
AntonWennbom <77405279+AntonWennbom@users.noreply.github.com>
2026-09-11 13:31:29 +0200

    Add Asset class

---
+++ ItAssetManagement.Domain/Assets/Asset.cs
@@ -0,0 +1,10 @@
+﻿namespace ItAssetManagement.Domain.Assets;
+
+internal class Asset
+{
+    public Guid AssetId { get; private set; }
+    public string AssetName { get; private set; }
+    public string SerialNumber { get; private set; }
+    public EAssetStatus Status { get; private set; }
+
+}
