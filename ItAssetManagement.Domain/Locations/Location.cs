5d2013226793f3a1fea0211e707e9196db329ef2
Ghraz-hub <tomas.dan.carlsson@hotmail.com>
2026-09-11 13:50:47 +0200

    Add Location model and Value objects

---
+++ ItAssetManagement.Domain/Locations/Location.cs
@@ -0,0 +1,23 @@
+﻿using ItAssetManagement.Domain.Locations.ValueObjects;
+
+namespace ItAssetManagement.Domain.Locations;
+
+public class Location(LocationName locationName, LocationCode locationCode)
+{
+    public Guid LocationId { get; init; } = GenerateId();
+    public LocationName LocationName { get; private set; } = locationName;
+    public LocationCode LocationCode { get; private set; } = locationCode;
+
+    private static Guid GenerateId () => Guid.NewGuid();
+
+    public void LocationNameChange(LocationName name)
+    {
+        LocationName = name;
+    }
+    public void LocationCodeChange(LocationCode code)
+    {
+        LocationCode = code;
+    }
+
+}
+
---
+++ ItAssetManagement.Domain/Locations/ValueObjects/LocationCode.cs
@@ -0,0 +1,18 @@
+﻿namespace ItAssetManagement.Domain.Locations.ValueObjects;
+
+public record LocationCode
+{
+    public string Value { get; }
+    public LocationCode(string value , int minLenght = 6)
+    {
+        if(string.IsNullOrWhiteSpace(value))
+            throw new ArgumentException("Location code is required.", nameof (value));
+
+        var normalizedValue = value.Trim().Replace("-", "");
+
+        if (normalizedValue.Length < minLenght)
+            throw new ArgumentException($"Location code must contain at least {minLenght} characters.", nameof(value));
+
+        Value = normalizedValue;
+    }
+}
