e084dd04e97f40e747f73f709570c227fc502293
Ghraz-hub <tomas.dan.carlsson@hotmail.com>
2026-09-11 14:03:09 +0200

    Add InMemoryLocationRepository and ILocationRepository

---
+++ ItAssetManagement.Domain/Locations/ILocationRepository.cs
@@ -0,0 +1,8 @@
+﻿namespace ItAssetManagement.Domain.Locations;
+
+public interface ILocationRepository
+{
+    bool AddLocation(Location location);
+
+    IReadOnlyList<Location> GetLocations();
+}
---
+++ ItAssetManagement.Infrastructure/Locations/InMemory/InMemoryLocationRepository.cs
@@ -0,0 +1,25 @@
+﻿using ItAssetManagement.Domain.Locations;
+
+namespace ItAssetManagement.Infrastructure.Locations.InMemory;
+
+public class InMemoryLocationRepository : ILocationRepository
+{
+    private readonly List<Location> _locations = [];
+
+    public bool AddLocation(Location location)
+    {
+        if (location is null)
+            throw new ArgumentNullException("Location is empty and cannot be added.");        
+        else
+        {
+            _locations.Add(location);
+            return true;
+        }
+
+    }
+
+    public IReadOnlyList<Location> GetLocations()
+    {
+        return _locations;
+    }
+}
